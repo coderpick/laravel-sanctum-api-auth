97334a6597474dcd94c43b0898bfe3714e07c027
hafizur <coderpickbd@gmail.com>
2022-09-12 23:49:07 +0600

    sanctum install & HttpResponse trait create

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\HttpResponses;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+}
---
+++ app/Traits/HttpResponses.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+trait HttpResponses
+{
+    protected function success($data, $message, $code = 200)
+    {
+        return response()->json([
+            'status' => 'Request was successful',
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    protected function error($data, $message, $code)
+    {
+        return response()->json([
+            'status' => 'Error was occurred',
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+}
